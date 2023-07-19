54784841018d1b33859e2744a6d334d75e0936c3
aishipaul <88893512+aishipaul@users.noreply.github.com>
2023-07-20 01:54:47 +0530

    .

---
+++ public/services.js
@@ -0,0 +1,6 @@
+const services = [
+    {id:2, name:'WEB DEVELOPMENT', description:'I create beautiful iterfaces with simple HTML, CSS, & JavaScript and also frameworks like Angular and ReactJS', image:'../src/assets/icons/code.png'},
+    {id:5, name:'NPM AND NODEJS', description:'I have core understanding of NPM. I can also develop general purpose applications with NodeJS', image:'../src/assets/icons/javascript.png'},
+]
+
+export default services;
